7f815fb29893bc70ed1780aed366b0fa72c4509f
Renan Cakirerk <renan.cakirerk@gmail.com>
2011-10-23 20:55:12 +0300

    Add template admin pages, trying to implement signIn method

--- give2get/app/controllers/Application.java
+++ give2get/app/controllers/Application.java
@@ -3,17 +3,24 @@ package controllers;
 import play.*;
 import play.mvc.*;
 
+import play.libs.*;
+import play.cache.*;
+
 import java.util.*;
 
 import models.*;
-
-import controllers.Security;
-
 
 
 @With(Secure.class)
 public class Application extends Controller {
 
+	public static void captcha(String id) {
+	    Images.Captcha captcha = Images.captcha();
+	    String code = captcha.getText("#222222");
+	    Cache.set(id, code, "10mn");
+	    renderBinary(captcha);
+	}
+	
     public static void index() {
     	List users = User.find("order by id desc").fetch();
         render(users);
@@ -22,5 +29,9 @@ public class Application extends Controller {
     public static void signUp(String name, String surname, String username, String password, String email) {
     	User user = new User(name, surname, username, password, email).save();
     	renderJSON(user);
+    }
+    
+    public static void signIn() {
+    	
     }
 }
--- give2get/app/controllers/Security.java
+++ give2get/app/controllers/Security.java
@@ -9,6 +9,22 @@ public class Security extends Secure.Security {
 		//return User.connect(username, password) != null;
 		return true;
     }
-    
+	
+	static void onDisconnected() {
+	    Application.index();
+	}
+	
+	static void onAuthenticated() {
+	    Admin.index();
+	}
+	
+	//To create an admin profile, you just need to override the check() method in the controllers.
+	static boolean check(String profile) {
+	    if("admin".equals(profile)) {
+	        return User.find("byEmail", connected()).<User>first().status == "admin";
+	    }
+	    return false;
+	}
+
 
 }
