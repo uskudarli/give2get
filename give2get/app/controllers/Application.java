6be95774052399d2bf3b3b20bf7e3f4e12aa512b
Renan Cakirerk <renan.cakirerk@gmail.com>
2011-10-23 17:26:35 +0300

    Modify controler, Add view

--- give2get/app/controllers/Application.java
+++ give2get/app/controllers/Application.java
@@ -10,7 +10,12 @@ import models.*;
 public class Application extends Controller {
 
     public static void index() {
-        render();
+    	List users = User.find("order by id desc").fetch();
+        render(users);
     }
 
+    public static void signUp(String name, String surname, String username, String password) {
+    	User user = new User(name, surname, username, password).save();
+    	renderJSON(user);
+    }
 }
